c85cb1c5ea7bffb0a74c4ca544bbfffb4fa6f78d
Darshan Shakya <darshanshakya111@gmail.com>
2025-02-04 21:28:15 +0545

    Minor changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,26 @@
 <x-app-layout>
-    <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 flex justify-between text-gray-900">
+                <span class="text-xl">
+                    @php
+                        $time = date('H');
+                        if ($time < 12) {
+                            echo 'Good Morning!';
+                        } elseif ($time < 17) {
+                            echo 'Good Afternoon!';
+                        } else {
+                            echo 'Good Evening!';
+                        }
+                    @endphp
+                    {{ Auth::user()->name }}
+                </span>
+                <span class="text-xl">
+                    Today is
+                    @php
+                        echo date('l, jS \o\f F, Y');
+                    @endphp
+                </span>
             </div>
         </div>
     </div>
