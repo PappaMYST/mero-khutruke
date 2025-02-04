76dd9a41de9bdce449bd8e9fc62fedcde9899fe9
Darshan Shakya <darshanshakya111@gmail.com>
2025-02-04 21:26:49 +0545

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.landing');
 });
-
-Route::resource('categories', CategoryController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('transaction', CategoryController::class);
+    Route::resource('budgets', CategoryController::class);
+    Route::resource('accounts', CategoryController::class);
+    Route::resource('transfers', CategoryController::class);
+
 });
 
 require __DIR__ . '/auth.php';
